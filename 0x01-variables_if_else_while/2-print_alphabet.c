ea46ba6089c826f97b5b135fe7d06900ca67cfa7
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 14:29:53 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,21 +3,23 @@
 /**
  * main - Entry point
  *
- * Description: This is the main function of the program.
+ * Description: This is the main function of the program. 
  * It prints the lowercase
  * alphabet from 'a' to 'z' followed by a newline character.
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-	char letter = 'a'; 
+int main(void)
+{
+    char letter = 'a'; 
 
-	while (letter <= 'z') {
-		putchar(letter);
-		letter++;
-	}
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
