5d6e487ea8133ba18f0bd97715314e8c83b02551
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 16:59:24 +0100

    Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-/**
-*
-*main : print numbers 0 to 9
-*
-*result 0 (success)
-*/
 
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers 0 to 9
+ *
+ * Return: 0 (success)
+ */
+int main(void)
 {
     int n;
 
-    for (n = 0; n < 10; n++) 
+    for (n = 0; n < 10; n++)
     {
-        putchar('0' + n); 
+        putchar('0' + n);
     }
 
     putchar('\n');
 
-    return 0;
+    return (0);
 }
