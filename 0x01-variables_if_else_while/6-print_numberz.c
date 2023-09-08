0a115a5b8593a421f057e8a4582c750269cd85c3
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 16:49:26 +0100

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,14 @@
 
 int main(void) 
 {
-	int n;
+    int n;
 
-	for (n = 0; n < 10; n++) 
-	{
-		putchar('0' + n);
-	}
+    for (n = 0; n < 10; n++) 
+    {
+        putchar('0' + n);
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return 0;
+    return (0); 
 }
