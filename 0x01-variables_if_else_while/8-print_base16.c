2e1f3f99eb289f40e3b7cb8989333496967313d9
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 17:30:44 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: Print alphabets backwards
+ * Description: Print numbers from 0 to 10 
+ * and lowercase letters from 'a' to 'e'
  *
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-  char x,z;
-  for(x=0,x<=10,x++)
-    {
-     putchar('0'+x);
-    }
-  for(z='a', z<=5, z++)
-    {
-    putchar(z);
-    }
-  putchar(\n);
-  return (0);
+	int x, z;
+
+	for (x = 0; x <= 10; x++)
+	{
+		putchar('0' + x);
+	}
+
+	for (z = 'a'; z <= 'e'; z++)
+	{
+		putchar(z);
+	}
+
+	putchar('\n');
+	return (0);
 }
