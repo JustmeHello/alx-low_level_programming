cf38424ff29419b8b999d950b1b0a3cc5976d5a0
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 17:27:39 +0100

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
-...
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabets backwards
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+  char x,z;
+  for(x=0,x<=10,x++)
+    {
+     putchar('0'+x)
+    }
+  for(z='a', z<=5, z++)
+    {
+    putchar(z);
+    }
+  putchar(\n);
+  return (0);
+}
