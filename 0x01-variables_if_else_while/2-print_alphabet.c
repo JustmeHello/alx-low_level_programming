9506744344861b52649a2b34197e806858552a66
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 14:01:20 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    char letter = 'a'; // Start with the letter 'a'
+    char letter = 'a'; /
 
-    // Loop through the alphabet and print each letter
-    while (letter <= 'z') {
+        while (letter <= 'z') {
         putchar(letter);
         letter++;
     }
 
-    // Print a newline character to move to the next line
     putchar('\n');
 
     return 0;
