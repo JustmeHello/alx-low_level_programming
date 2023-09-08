9d165a4132c72514002543c55676437f2aa7c45f
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 18:30:25 +0100

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,20 +15,20 @@ int main(void)
 	for (i = 0; i <= 7; i++)
 	{
 		for (j = i + 1; j <= 8; j++)
-      for (k = j + 1; k <= 9; k++)
-      {
-		
-			putchar(i + '0');
-			putchar(j + '0');
-      putchar(k + '0');
-
-			if (i != 7 || j != 8  || k != 9)
+		{
+			for (k = j + 1; k <= 9; k++)
 			{
-				putchar(',');
-				putchar(' ');
-      
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-		  }
+		}
 	}
 
 	putchar('\n');
