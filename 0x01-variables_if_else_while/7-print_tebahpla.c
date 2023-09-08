a58c398c813f25d19b9cdb5bbd81af9fbec4617a
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 17:15:42 +0100

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
 /**
-*
-*main - entry point
-*description : print alphabets backwards
-*return 0 (success)
-*/
+ * main - Entry point
+ * Description: Print alphabets backwards
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-  char letter;
-  for (letter= 'z', letter>='a', letter--)
-         putchar(letter);
-  putchar(\n);
-  return (0);
+    char letter;
+    for (letter = 'z'; letter >= 'a'; letter--)
+    {
+        putchar(letter);
+    }
+    putchar('\n');
+    return (0);
 }
