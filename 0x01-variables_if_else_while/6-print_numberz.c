7bb2ebd1f259437ed6c9df20e7787b666f475222
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 16:52:44 +0100

    Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,10 @@ int main(void)
 
     for (n = 0; n < 10; n++) 
     {
-        putchar('0' + n); // Print the character representation of the number
+        putchar('0' + n); /* Print the character representation of the number */
     }
 
     putchar('\n');
 
-    return 0; // Removed parentheses around the return value
+    return 0;
 }
