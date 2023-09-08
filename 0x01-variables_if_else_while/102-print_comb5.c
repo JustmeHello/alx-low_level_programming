d40366ef91b334e0200a3fe18a9f2cd05aa80349
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 18:46:54 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,41 @@
-...:
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all possible different combinations of three digits
+ * separated by a comma and a space.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+      putchar(' ');
+		{
+			for (k = 0; k <= 9; k++)
+			{
+        for (l = 0; l <= 9; l++)
+        {
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+        putchar(l + '0');
+
+				if (i != 9 || j != 9 || k != 9 || l!= 9)
+				{
+					putchar(',');
+					putchar(' ');
+        }
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
