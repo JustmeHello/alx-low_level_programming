c811fc89033fb9645ecb7699d4ee69af933f00dc
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 18:53:48 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,20 +15,20 @@ int main(void)
     {
         for (j = i + 1; j <= 99; j++)
         {
-            putchar((i / 10) + '0'); // Print the tens digit of the first number
-            putchar((i % 10) + '0'); // Print the ones digit of the first number
-            putchar(' '); // Print a space
-            putchar((j / 10) + '0'); // Print the tens digit of the second number
-            putchar((j % 10) + '0'); // Print the ones digit of the second number
+            putchar((i / 10) + '0'); /* Print the tens digit of the first number */
+            putchar((i % 10) + '0'); /* Print the ones digit of the first number */
+            putchar(' '); /* Print a space */
+            putchar((j / 10) + '0'); /* Print the tens digit of the second number */
+            putchar((j % 10) + '0'); /* Print the ones digit of the second number */
 
             if (i != 98 || j != 99)
             {
-                putchar(','); // Print a comma
-                putchar(' '); // Print a space
+                putchar(','); /* Print a comma */
+                putchar(' '); /* Print a space */
             }
         }
     }
 
-    putchar('\n'); // Print a newline character
+    putchar('\n'); /* Print a newline character */
     return (0);
 }
