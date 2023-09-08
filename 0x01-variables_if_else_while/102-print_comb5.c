c380f8668494ae8fc4b3c6f241a1fbe568b65ba2
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 18:55:08 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,26 +9,26 @@
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i <= 99; i++)
-    {
-        for (j = i + 1; j <= 99; j++)
-        {
-            putchar((i / 10) + '0'); /* Print the tens digit of the first number */
-            putchar((i % 10) + '0'); /* Print the ones digit of the first number */
-            putchar(' '); /* Print a space */
-            putchar((j / 10) + '0'); /* Print the tens digit of the second number */
-            putchar((j % 10) + '0'); /* Print the ones digit of the second number */
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0'); /* Print the tens digit of the first number */
+			putchar((i % 10) + '0'); /* Print the ones digit of the first number */
+			putchar(' '); /* Print a space */
+			putchar((j / 10) + '0'); /* Print the tens digit of the second number */
+			putchar((j % 10) + '0'); /* Print the ones digit of the second number */
 
-            if (i != 98 || j != 99)
-            {
-                putchar(','); /* Print a comma */
-                putchar(' '); /* Print a space */
-            }
-        }
-    }
+			if (i != 98 || j != 99)
+			{
+				putchar(','); /* Print a comma */
+				putchar(' '); /* Print a space */
+			}
+		}
+	}
 
-    putchar('\n'); /* Print a newline character */
-    return (0);
+	putchar('\n'); /* Print a newline character */
+	return (0);
 }
