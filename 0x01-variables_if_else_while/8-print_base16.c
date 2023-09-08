e89fdb8ce8268d1377df742403afb896b19db751
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 17:31:50 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,25 +3,25 @@
 /**
  * main - Entry point
  *
- * Description: Print numbers from 0 to 10 
- * and lowercase letters from 'a' to 'e'
+ * Description: Print numbers from 0 to 10 and lowercase letters from 'a' to 'e'
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int x, z;
+    int x;
+    char z;
 
-	for (x = 0; x <= 10; x++)
-	{
-		putchar('0' + x);
-	}
+    for (x = 0; x <= 10; x++)
+    {
+        putchar('0' + x);
+    }
 
-	for (z = 'a'; z <= 'e'; z++)
-	{
-		putchar(z);
-	}
+    for (z = 'a'; z <= 'e'; z++)
+    {
+        putchar(z);
+    }
 
-	putchar('\n');
-	return (0);
+    putchar('\n');
+    return (0);
 }
