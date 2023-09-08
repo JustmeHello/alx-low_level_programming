07bb6744d86b7c6703708150e555100966c4e37d
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 18:13:36 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,25 +3,29 @@
 /**
  * main - Entry point
  *
- * Description: Prining double digits 
+ * Description: Printing double-digit numbers with different digits separated by commas and spaces
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int n,m;
+    int n, m;
 
-	for (n = 0; n <= 9; n++, m = 0, m <= 9, m = ++, n != m)
-	{
-		putchar('00' + nm);
+    for (n = 0; n <= 9; n++)
+    {
+        for (m = n + 1; m <= 9; m++)
+        {
+            putchar('0' + n);
+            putchar('0' + m);
 
-		if (n < 9 && m < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+            if (n < 8 || m < 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-	putchar('\n');
-	return (0);
+    putchar('\n');
+    return (0);
 }
