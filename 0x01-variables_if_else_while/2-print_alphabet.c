179ab6e7b930a2239c56e4b0daaaf61847948517
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 14:33:55 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,15 +11,15 @@
  */
 int main(void)
 {
-	char letter = 'a'; 
+    char letter = 'a'; 
 
-	while (letter <= 'z')
-	{
-		putchar(letter);
-		letter++;
-	}
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
