ef2d48996060643bffc63f23cf02eed65e26fc2a
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 17:18:10 +0100

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,16 +2,20 @@
 
 /**
  * main - Entry point
+ *
  * Description: Print alphabets backwards
+ *
  * Return: 0 (Success)
  */
 int main(void)
 {
     char letter;
+
     for (letter = 'z'; letter >= 'a'; letter--)
     {
         putchar(letter);
     }
+
     putchar('\n');
     return (0);
 }
