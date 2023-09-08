26a4cdd4b7a034e670df3b2b1e970d42c08c2333
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 17:50:35 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    for (n = 0; n <= 9; n++)
-    {
-        putchar('0' + n);
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
 
-        if (n < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
