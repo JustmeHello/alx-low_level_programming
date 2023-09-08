73cbeaeffab5f947077de4e0b3713611af1861f1
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-08 17:46:03 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,18 @@
-...
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers 0 to 9 seperated with , 
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+  int n;
+  for (n=0, n<=0 , n++)
+    {
+    putchar('0'+n+' ,');
+    }
+  putchar(\n);
+  return(0);
+}
