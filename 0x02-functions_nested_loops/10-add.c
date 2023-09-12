af3df731a73e75b96ea7f34de7a04f5f7fd44468
JustmeHello <aribat@proton.me>
2023-09-12 05:44:26 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
+int main(void) {
+    int a;
+    int b;
+    
+    
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+    
+
+    return 0;
+}
