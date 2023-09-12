d81fbc1fa761c12ef358cbab021d9e823f650290
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 15:57:53 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,24 +3,21 @@
 int main(void) {
     long long int fib[3];
     long long int sum = 0;
-    
-    fib[0] = 1; 
-    fib[1] = 1; 
-    fib[2] = 0; 
 
-    
+    fib[0] = 1;
+    fib[1] = 1;
+    fib[2] = 0;
+
     while (fib[1] <= 4000000) {
-        if (fib[1] % 2 == 0) { 
-            sum += fib[1]; 
+        if (fib[1] % 2 == 0) {
+            sum += fib[1];
         }
-        fib[2] = fib[0] + fib[1]; 
-        fib[0] = fib[1]; 
+        fib[2] = fib[0] + fib[1];
+        fib[0] = fib[1];
         fib[1] = fib[2];
     }
 
-    
     printf("%lld\n", sum);
 
     return 0;
 }
-
