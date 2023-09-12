52496ee94df419d767e04fca6c2e878c3d96a6af
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 17:26:26 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(void) {
-    int i;
-    int l;
-    scanf("%d", &i);
-    l = i % 10;
-    
-    printf("%d\n", l);
-        
+#include "main.h"
+/**
+*jack_bauer - prints each minute of the day
+*
+*Return:returns 0
+*/
+void jack_bauer(void)
+{
+int min, hour;
+for (hour = 0; hour <= 23; hour++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar(10);
+}
+}
 
-    return 0;
 }
