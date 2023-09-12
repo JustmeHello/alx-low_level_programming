a903c58b717cd1a1ac526c0eac9d4983cbe644f3
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 17:29:02 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,41 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
+*print_times_table - prints the  times table with
+*parameter
+*@n: parameter
+*Return: returns nothing
+*/
 void print_times_table(int n)
 {
-	int i, j, k;
+int digit, mult, result;
+if (n <= 15 && n >= 0)
+{
+for (digit = 0; digit <= n; digit++)
+{
+_putchar('0');
 
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar(' ');
-					putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar((k / 10) + '0');
-					putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					putchar(',');
-					putchar(' ');
-					putchar((k / 100) + '0');
-					putchar(((k / 10) % 10) + '0');
-					putchar((k % 10) + '0');
-				}
-			}
-			putchar('\n');
-		}
-	}
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 99)
+_putchar(' ');
+
+if (result <= 9)
+_putchar(' ');
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar((result / 10) % 10 + '0');
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
