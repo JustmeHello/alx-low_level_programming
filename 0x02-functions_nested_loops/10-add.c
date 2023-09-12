88f9b4aa48fe0f307a6a37373beabfbcc206cafc
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 17:27:50 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - Check the code
- *
- * Return: Always 0.
- */
-int main(void) {
-    int a;
-    int b;
-    
-    
-    scanf("%d %d", &a, &b);
-    printf("%d\n", a + b);
-    
-
-    return 0;
+*add - sums up two numbers
+*@n:first integer
+*@m:second integer
+*Return: returns 0
+*/
+int add(int n, int m)
+{
+return (n + m);
 }
