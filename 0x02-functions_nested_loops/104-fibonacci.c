30b264489418937ac231a85c2625c606c56282c7
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 15:53:44 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    unsigned long long a = 1, b = 2;
-    printf("%llu, %llu, ", a, b);
+    unsigned int a = 1, b = 2;
+    printf("%u, %u, ", a, b);
 
     for (int i = 3; i <= 98; i++) {
-        unsigned long long next = a + b;
-        printf("%llu", next);
+        unsigned int next = a + b;
+        printf("%u", next);
 
         if (i < 98) {
             printf(", ");
