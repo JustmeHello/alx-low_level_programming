c6f1521584a0e34be2cd22d0407ea8be84ffaf76
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 20:37:16 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Check the code
- *
- * Return: Always 0.
- */
-int main(void) {
-    int h;
-    int m;
-    
-    for (h = 0; h <= 23; h++) {
-        for (m = 0; m <= 59; m++) {
-            printf("%02d:%02d\n", h, m);  
-        }
-    }
-    
-    return 0;
+*jack_bauer - prints each minute of the day
+*
+*Return:returns 0
+*/
+void jack_bauer(void)
+{
+int min, hour;
+for (hour = 0; hour <= 23; hour++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar(10);
 }
-
+}
+}
