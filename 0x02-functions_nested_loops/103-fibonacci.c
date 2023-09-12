639e409f812d6aa15494546ded1c2c4e4e282042
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 16:00:36 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Description: This program calculates the sum of even Fibonacci numbers
+ * not exceeding 4,000,000.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     long fib[3];
     long sum = 0;
 
@@ -8,8 +17,10 @@ int main(void) {
     fib[1] = 1;
     fib[2] = 0;
 
-    while (fib[1] <= 4000000) {
-        if (fib[1] % 2 == 0) {
+    while (fib[1] <= 4000000)
+    {
+        if (fib[1] % 2 == 0)
+        {
             sum += fib[1];
         }
         fib[2] = fib[0] + fib[1];
@@ -19,5 +30,5 @@ int main(void) {
 
     printf("%ld\n", sum);
 
-    return 0;
+    return (0);
 }
