91f76dff5249867640b26705e6938c22f7ac33ca
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 20:27:49 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,13 +11,13 @@
  */
 void print_alphabet(void)
 {
-    char letter = 'a';
+char letter = 'a';
 
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
 
-    putchar('\n');
+putchar('\n');
 }
