0a0795af946abc50d90110064edec0f14075199d
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 17:23:52 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <ctype.h>
-int main(void) {
-    char c;
-    scanf("%c", &c);
+#include "main.h"
+/**
+*_isalpha - controls if a character is alphabetical
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
 
-    if (isalpha(c)) {
-        printf("1\n");
-    } else {
-        printf("0\n");
-    }
-
-    return 0;
+return (0);
 }
