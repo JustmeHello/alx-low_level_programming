e242172ab8bd422129b773c1b33d6ef729cbf185
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-12 17:08:04 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-int main(void) {
-    printf("_putchar\n");
+int main(void)
+{
+    printf("_putchar");
     return (0);
 }
