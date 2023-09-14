e1c33e00ff39ce54777078fdcdcae5ee8f8b981a
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-14 20:23:41 +0100

    distinguishing prime numbers from composite numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,33 +1,31 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Description: Prints the largest prime factor of a 612852475143
- *
- * Return: Always 0 (Success)
- */
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
+*/
+
 int main(void)
 {
-    long int n = 612852475143;
-    long int div = 2, maxFact = 0;
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
 
-    while (n != 0)
-    {
-        if (n % div != 0)
-        {
-            div = div + 1;
-        }
-        else
-        {
-            maxFact = n;
-            n = n / div;
-            if (n == 1)
-            {
-                printf("%ld is the largest prime factor!\n", maxFact);
-                break;
-            }
-        }
-    }
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
 
-    return (0);
+}
+}
+printf("%ld\n", number);
+return (0);
 }
