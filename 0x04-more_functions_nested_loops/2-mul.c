71f633c3ac0c9d75051b47c6c31585eb1ee11b72
JustmeHello <aribat@proton.me>
2023-09-14 01:37:08 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Muliply two numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{       
+int a;  
+int b;
+int mul;
+
+scanf("%d %d", &a, &b);
+mul = a * b;
+printf("%d\n",mul);
+return 0;
+}
