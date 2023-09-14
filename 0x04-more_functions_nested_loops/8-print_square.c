65bfc1ab304e247aa7d757594aef2d236fdc899c
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-14 20:21:04 +0100

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,25 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Description: turn number to matrix in #
- *
- * Return: Always 0 (Success)
- */
-int main()
+*print_square - prints squares
+*@size: parameter
+*Return:returns nothing
+*/
+void print_square(int size)
 {
-    int size, i,n;
-    scanf("%d", &n);
-    if (n <= 0)
-    {
-        putchar('\n');
-        return 0;
-    }
+int inc1, inc2;
+if (size > 0)
+{
+for (inc1 = 0; inc1 < size; inc1++)
+{
+for (inc2 = 0; inc2 < (size - 1); inc2++)
+_putchar('#');
 
-    for (size = 1; size <= n; size++)
-    {
-        for (i = 1; i <= n; i++)
-        {
-            putchar('#');
-        }
-
-        putchar('\n');
-    }
-    return 0;
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
