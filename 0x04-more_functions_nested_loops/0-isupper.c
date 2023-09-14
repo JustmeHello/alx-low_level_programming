604cb85447fdc773d1ca0ce1871f132eec06ac2d
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-14 09:11:28 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,17 @@
-...
+#include "main.h"
+/**
+* main - causes an infinite loop
+* description : check if char is upper case
+* Return: 0
+*/
+
+int main(void) 
+{
+char a;
+scanf("%c", &a);
+if (isupper(a))
+printf("1");
+else
+printf('0");
+return 0;
+}
