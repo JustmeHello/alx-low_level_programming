52db260bbf335f6957a68dbdd900736d25d18cce
JustmeHello <aribat@proton.me>
2023-09-14 01:02:54 -0700

    up

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,31 @@
+
+/**
+*File:main.h
+*
+*Desc:header file containing functions declaration
+*/
+
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
