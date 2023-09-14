5989922182f12cf87328c8d2d8ce49c29521c768
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-14 09:30:45 +0100

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,20 @@
-...
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Checks if a character is a digit.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+    scanf(" %c", &c);
+
+    if (isdigit(c))
+        printf("1\n");
+    else
+        printf("0\n");
+
+    return 0;
+}
