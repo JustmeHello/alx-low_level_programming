db6e8a10d558ebb88eab7f7d348abcdfb5ce250c
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 15:58:06 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,45 @@
 #include "main.h"
 
-
-
-int _atoi(const char *str) {
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
     int result = 0;
-    int sign = 1; 
+    int sign = 1;
+    int digit;
+
+    if (s == NULL)
+        return 0;
 
     
-    if (*str == '-') {
-        sign = -1;
-        str++;
+    while (*s == '-' || *s == '+')
+    {
+        if (*s == '-')
+            sign *= -1;
+        s++;
     }
 
-   
-    while (*str != '\0') {
-        if (*str >= '0' && *str <= '9') {
-            result = result * 10 + (*str - '0');
-        } else {
-            
-            printf("Invalid character in the string: %c\n", *str);
-            return 0; 
+
+    while (*s >= '0' && *s <= '9')
+    {
+        digit = *s - '0';
+
+        
+        if (result > (INT_MAX - digit) / 10)
+        {
+            if (sign == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
         }
-        str++;
+
+        result = result * 10 + digit;
+        s++;
     }
 
-    
     return result * sign;
 }
