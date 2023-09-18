809897aed555f826d766018703e8880a5f80b011
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 13:53:21 +0100

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,13 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            putchar(str[i]);
-        i++;
-    }
-    putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
 }
