85afb92c14ff545da24986a1ebd1ec25152b9e46
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 13:24:06 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*prints every other character of a string
+* starting with the first character
+*followed by a new line.
+*/
+void puts2(char *str)
+{
+        while (*str)
+        {
+                putchar(*str);
+                str =str + 2;
+        }
+        putchar('\n');
+}
