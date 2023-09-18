e2371a014d58984138da02ef6607ff4e5e088a98
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 12:04:30 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,12 @@
  * 
  */
 int _strlen(char *s) {
-    int length = 0;
+int length = 0;
 
-    while (*s != '\0') {
-        length++;
-        s++;
-    }
+while (*s != '\0') {
+	length++;
+	s++;
+	}
 
-    return (length);
+return (length);
 }
