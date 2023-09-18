3b9b7c7d25570a15ca98f3eba4cc1c3a9e526a27
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 13:41:22 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * prints every other character of a string
- * starting with the first character
- * followed by a new line.
+ * puts2 - Prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: The string to print.
  */
 void puts2(char *str)
 {
-    while (*str != '\0')
-    {
-        putchar(*str);
-        str = str + 2;
-    }
-    putchar('\n');
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str = str + 2;
+	}
+	putchar('\n');
 }
