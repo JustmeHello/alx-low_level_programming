262e18ee8d53cbdf22dade9961add79504063e7b
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 12:44:47 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,10 +6,19 @@
  */
 void print_rev(char *s)
 {
-	while (*s = '\0')
+	int length = 0;
+	
+	
+	while (s[length] != '\0')
 	{
-		putchar(*s);
-		str--;
+		length++;
 	}
+	
+	
+	for (int i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	
 	putchar('\n');
 }
