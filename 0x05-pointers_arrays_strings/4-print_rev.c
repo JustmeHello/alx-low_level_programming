0a601928bc3db501654792a95e6877c4ea0cecf3
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 12:46:55 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,20 @@
  */
 void print_rev(char *s)
 {
-	int length = 0;
-	
-	
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-	
-	
-	for (int i = length - 1; i >= 0; i--)
-	{
-		putchar(s[i]);
-	}
-	
-	putchar('\n');
+    int length = 0;
+
+    // Calculate the length of the string
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    // Print the string in reverse
+    int i;
+    for (i = length - 1; i >= 0; i--)
+    {
+        putchar(s[i]);
+    }
+
+    putchar('\n');
 }
