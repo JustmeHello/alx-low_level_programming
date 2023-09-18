490af54c7cd5e041b624978b3cc353cc2c107354
JustmeHello <aribat@proton.me>
2023-09-18 03:30:19 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * 
+ * 
+ *
+ * Description: This function swaps the values of two integers.
+ * 
+ */
+void swap_int(int *a, int *b)
+{
+int *temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
