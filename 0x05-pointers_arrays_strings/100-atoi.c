cdeab023173b79f2bfb0bc7415be1ddf3aaa4977
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 16:02:36 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h> 
 
 /**
  * _atoi - Converts a string to an integer.
@@ -15,7 +16,7 @@ int _atoi(char *s)
     if (s == NULL)
         return 0;
 
-    
+  
     while (*s == '-' || *s == '+')
     {
         if (*s == '-')
@@ -23,7 +24,7 @@ int _atoi(char *s)
         s++;
     }
 
-
+   
     while (*s >= '0' && *s <= '9')
     {
         digit = *s - '0';
