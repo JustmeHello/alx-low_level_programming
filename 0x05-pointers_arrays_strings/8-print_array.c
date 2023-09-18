44fcc8385758ef33553930f69b22081bdacfb3ba
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 14:51:35 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,18 +2,18 @@
 
 void print_array(int *a, int n)
 {
-    int i; 
-
-    if (n <= 0) {
+    if (n <= 0)
+    {
         printf("\n");
         return;
     }
 
-    printf("%d", a[0]); 
+    printf("%d", a[0]); // Print the first element
 
-    for (i = 1; i < n; i++) {
-        printf(", %d", a[i]);  
+    for (int i = 1; i < n; i++)
+    {
+        printf(", %d", a[i]); // Print the remaining elements with a comma and space
     }
 
-    printf("\n");
+    printf("\n"); // Print a new line at the end
 }
