b4380cdbc66ea278809f6b341e26a40f0ae9ce90
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 11:56:19 +0100

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,14 @@
  * returns the length of a string.
  * 
  */
-int _strlen(char *s)
-{
-strlen(*s);
+int _strlen(char *s) {
+    int length = 0;
+
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
+
+    return length;
+}
 }
