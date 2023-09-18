ae1d5a4c2496548e74d14b7254688ab912354b6c
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 14:43:43 +0100

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+    if (n <= 0) {
+        printf("\n");
+        return;
+    }
+
+    printf("%d", a[0]); 
+
+    for (int i = 1; i < n; i++) {
+        printf(", %d", a[i]); 
+    }
+
+    printf("\n"); 
+}
