49a388219d620c4e1d285d9baf16308831684c10
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-18 16:20:23 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,38 +9,38 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int digit;
+	int result = 0;
+	int sign = 1;
+	int digit;
 
-    if (s == NULL)
-        return (0);
+	if (s == NULL)
+		return (0);
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit = *s - '0';
 
-           
-            if (result > (INT_MAX - digit) / 10)
-            {
-                if (sign == 1)
-                    return (INT_MAX);
-                else
-                    return (INT_MIN);
-            }
+			
+			if (result > (INT_MAX - digit) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
 
-            result = result * 10 + digit;
-        }
-        else if (result != 0) 
-        {
-            break;
-        }
-        s++;
-    }
+			result = result * 10 + digit;
+		}
+		else if (result != 0)
+		{
+			break;
+		}
+		s++;
+	}
 
-    return (result * sign);
+	return (result * sign);
 }
