92c7621eede15862da750ef68a50ce5fe9c4380d
JustmeHello <aribat@proton.me>
2023-09-18 05:15:25 -0700

    up

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+    int length = 0;
+    int i;
+
+    /* Calculate the length of the string */
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    /* Print the string in reverse */
+    for (i = length - 1; i >= 0; i--)
+    {
+        putchar(s[i]);
+    }
+
+    putchar('\n');
+}
