61a8bdd3c53a8e9b59d58a6558ac0781c52f646b
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-20 23:23:21 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+char *string_toupper(char *)
+{
+  int length = strlen(str);
+  for(int i = 0; i < length ; i++)
+    if(islower(str[i])
+    {
+      str[i] = toupper(str[]i);
+    }
+}
