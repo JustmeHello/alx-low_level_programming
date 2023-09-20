9220b0ff4098cc35b3f7d3aaa993caf7d006f8cc
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-20 23:06:49 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void reverse_array(int *a, int n)
+{
+  int length = strlen(str);
+  int i; int j;
+  for(i=0; j=length -i; i < j; i++; j--)
+    {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+    }
+}
