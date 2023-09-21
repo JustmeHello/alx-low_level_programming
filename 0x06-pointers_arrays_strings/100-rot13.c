055b88cf1ee2db9d342a6781b3a1cc2996e25356
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-21 12:45:47 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,31 +6,36 @@
  *
  * Return: Returns the encoded string.
  */
-char *rot13(const char *src)
+char *rot13(char *str)
 {
-    if(src == NULL){
-      return NULL;
-    }
-  
-    char* result = malloc(strlen(src));
-    
-    if(result != NULL){
-      strcpy(result, src);
-      char* current_char = result;
-      
-      while(*current_char != '\0'){
-        
-        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
-          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
-            
-            *current_char -= 13;
-          }else{
-            
-            *current_char += 13;
-          }
-        }
-        current_char++;
-      }
-    }
-    return result;
+	int i = 0;
+	char *s = str;
+	char from[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char to[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*s)
+	{
+		char *pos = from;
+		char *newChar = to;
+		int found = 0;
+
+		while (*pos)
+		{
+			if (*s == *pos)
+			{
+				*s = *newChar;
+				found = 1;
+				break;
+			}
+			pos++;
+			newChar++;
+		}
+
+		if (found)
+			s++;
+		else
+			s++;
+	}
+
+	return (str);
 }
