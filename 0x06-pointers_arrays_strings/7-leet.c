ac60c6f86008bc68294694063b4b1caf8fe22516
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-21 11:16:58 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-char *leet(char *str)
-{
-    char *leetReplace = "aAeEoOtTlL";
-    char *leetWith = "4433007711";
+char *leet(char *str) {
+    char leetReplace[] = "aAeEoOtTlL";
+    char leetWith[] = "4433007711";
     
     for (int i = 0; str[i]; i++) {
         for (int j = 0; leetReplace[j]; j++) {
