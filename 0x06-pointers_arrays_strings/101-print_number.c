af272706ca297dab67c5e3c3f862c773cb648004
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-21 12:52:39 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,21 +6,21 @@
  */
 void print_number(int n)
 {
-	/* Handle negative numbers */
+	
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	/* Handle the case of a single digit */
+	
 	if (n < 10)
 	{
 		_putchar(n + '0');
 	}
 	else
 	{
-		/* Recursive call to print each digit */
+		
 		print_number(n / 10);
 		_putchar(n % 10 + '0');
 	}
