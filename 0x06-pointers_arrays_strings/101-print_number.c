1c8994992f3d21dc90b8c756d666799b91ed7b74
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-21 20:32:57 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@ void print_number(int n)
 {
     if (n < 0)
     {
-        _putchar('-');
+        putchar('-');
         n = -n;
     }
 
@@ -17,5 +17,5 @@ void print_number(int n)
         print_number(n / 10);
     }
 
-    _putchar((n % 10) + '0');
+    putchar((n % 10) + '0');
 }
