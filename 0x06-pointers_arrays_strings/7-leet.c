e6078d437d623a1fe647e075a73021e71dca7716
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-21 11:09:27 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
-char *leet(char *)
+char *leet(char *str)
 {
-for(int i = 0; str[i]; i++)
-{
-char currentChar = tolower(str[i]);
-  if (currentChar == 'a')
-    str[i] == '4';
-  if (currentChar == 'e')
-    str[i] == '3';
-  if (currentChar == 'o')
-    str[i] == '0';
-  if (currentChar == 't')
-    str[i] == '7';
-  if (currentChar == 'l')
-    str[i] == '7';
-}
+    char *leetReplace = "aAeEoOtTlL";
+    char *leetWith = "4433007711";
+    
+    for (int i = 0; str[i]; i++) {
+        for (int j = 0; leetReplace[j]; j++) {
+            if (str[i] == leetReplace[j]) {
+                str[i] = leetWith[j];
+                break;  // Exit the inner loop once a replacement is made
+            }
+        }
+    }
+    
+    return str;
 }
