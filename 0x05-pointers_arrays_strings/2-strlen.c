c200a541c8d0075cd675c119d9a097a74c600280
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-27 11:02:11 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,10 @@
  *
  * Return: Length of the string.
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 int _strlen(char *s)
 {
-<<<<<<< HEAD
+
 
 	int length = 0;
 	while (*s != '\0\){
@@ -18,27 +17,6 @@ int _strlen(char *s)
 			s++;
 	}
 	return 0;
-
-=======
-strlen(*s);
->>>>>>> 1b55564f50904a1344b11c1c38f7e84949ede88c
-=======
-int _strlen(char *s) {
-    int length = 0;
-=======
-int _strlen(char *s)
-{
-	int length = 0;
->>>>>>> de95b79bd405b78ed68a0ade7778cf6b9ac8ea8a
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-
-<<<<<<< HEAD
-    return length;
 }
 >>>>>>> b4380cdbc66ea278809f6b341e26a40f0ae9ce90
 =======
