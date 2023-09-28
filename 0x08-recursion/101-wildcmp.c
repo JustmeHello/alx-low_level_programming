ddc921149dc6b35d87251904af7261eca00a0d46
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-28 14:36:50 +0100

    Create 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+int wildcmp(char *s1, char *s2) {
+    if (*s1 == '\0' && *s2 == '\0') {
+        return 1;
+    }
+    if (*s2 == '*' || (*s1 == *s2 && *s1 != '\0')) {
+        return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1));
+    }
+    return 0;
+}
