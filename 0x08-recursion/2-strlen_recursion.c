ac46981dbe72aa2104785264b183a58f4340ba2e
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-28 11:47:32 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Returns the length of a string.
- * @s: The string to be printed.
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to find the length of.
+ *
+ * Return: The length of the string.
  */
-void _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-{
-return (0);
-}
-else
-{
-return (1 + _strlen_recursion(s + 1));
-}
+    if (*s == '\0')
+        return 0;
+
+    return 1 + _strlen_recursion(s + 1);
 }
