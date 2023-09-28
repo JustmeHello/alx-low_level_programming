5bea63bacf15fde83b54d5d0ae87fa1c49890e21
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-28 11:53:07 +0100

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to find the length of.
+ *
+ * Return: The length of the string.
+ */
+int factorial(int n)
+{
+	if (n <= 0)
+		return (-1);
+  else if (n == 1)
+    return (1);
+	return (factorial(n)*factorial(n-1));
+}
