f82a9e117f396556ed2ca5cb6066f11c8f438036
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-28 14:17:53 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,16 +9,15 @@
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-int is_palindrome_recursive(char *s, int start, int end) {
-    if (start >= end) {
-        return 1;
-    }
-    
-    if (s[start] == s[end]) {
-        return is_palindrome_recursive(s, start + 1, end - 1);
-    } else {
-        return 0;
-    }
+int is_palindrome_recursive(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] == s[end])
+		return is_palindrome_recursive(s, start + 1, end - 1);
+	else
+		return (0);
 }
 
 /**
@@ -27,7 +26,8 @@ int is_palindrome_recursive(char *s, int start, int end) {
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-int is_palindrome(char *s) {
-    int length = strlen(s);
-    return is_palindrome_recursive(s, 0, length - 1);
+int is_palindrome(char *s)
+{
+	int length = strlen(s);
+	return is_palindrome_recursive(s, 0, length - 1);
 }
