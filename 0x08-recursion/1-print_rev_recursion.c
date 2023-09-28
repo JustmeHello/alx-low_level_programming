46d81e50473a9359cf70864a7cdd920a1ff0a4f8
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-28 11:13:57 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {  
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-
-	_putchar(*s);
-	_print_rev_recursion(s - 1);
+	if (*s)
+  {
+    _print_rev_recursion(s + 1);
+    printf("%c", *str);
+  }
 }
