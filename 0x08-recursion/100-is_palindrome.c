b779082a082109c583993f149674b4d3e934e002
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-28 13:42:39 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,19 @@
 #include "main.h"
+#include <string.h>
 
+/**
+ * is_palindrome_recursive - Checks if a string is a palindrome recursively.
+ * @s: The string to check.
+ * @start: The starting index of the current substring.
+ * @end: The ending index of the current substring.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
 int is_palindrome_recursive(char *s, int start, int end) {
     if (start >= end) {
         return 1;
     }
+    
     if (s[start] == s[end]) {
         return is_palindrome_recursive(s, start + 1, end - 1);
     } else {
@@ -11,6 +21,12 @@ int is_palindrome_recursive(char *s, int start, int end) {
     }
 }
 
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
 int is_palindrome(char *s) {
     int length = strlen(s);
     return is_palindrome_recursive(s, 0, length - 1);
